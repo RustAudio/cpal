c2ed8467654b5ad7a1ca72f695985813e4b88b48
Pierre Krieger <pierre.krieger1708@gmail.com>
2014-12-16 16:45:45 +0100

    Add basic implementation for alsa

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
 #![feature(unsafe_destructor)]
 
-#[cfg(all(not(windows)))]
+#[cfg(all(not(windows), not(unix)))]
 use this_platform_is_not_supported;
 
 mod conversions;
 
+#[cfg(unix)]
+#[path="alsa/mod.rs"]
+pub mod cpal_impl;
 #[cfg(windows)]
 #[path="wasapi/mod.rs"]
 pub mod cpal_impl;
