efc4ee1f9207acbbcaa67aa958cfa9f543345e92
mitchmindtree <mitchell.nordine@gmail.com>
2018-02-05 13:26:02 +1100

    Merge match exprs in beep.rs example

--- examples/beep.rs
+++ examples/beep.rs
@@ -24,34 +24,28 @@ fn main() {
 
     event_loop.run(move |_, data| {
         match data {
-            cpal::StreamData::Output { buffer } => {
-                match buffer {
-                    cpal::UnknownTypeBuffer::U16(mut buffer) => {
-                        for sample in buffer.chunks_mut(format.channels as usize) {
-                            let value = ((next_value() * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
-                            for out in sample.iter_mut() {
-                                *out = value;
-                            }
-                        }
-                    },
-
-                    cpal::UnknownTypeBuffer::I16(mut buffer) => {
-                        for sample in buffer.chunks_mut(format.channels as usize) {
-                            let value = (next_value() * std::i16::MAX as f32) as i16;
-                            for out in sample.iter_mut() {
-                                *out = value;
-                            }
-                        }
-                    },
-
-                    cpal::UnknownTypeBuffer::F32(mut buffer) => {
-                        for sample in buffer.chunks_mut(format.channels as usize) {
-                            let value = next_value();
-                            for out in sample.iter_mut() {
-                                *out = value;
-                            }
-                        }
-                    },
+            cpal::StreamData::Output { buffer: cpal::UnknownTypeBuffer::U16(mut buffer) } => {
+                for sample in buffer.chunks_mut(format.channels as usize) {
+                    let value = ((next_value() * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
+                    for out in sample.iter_mut() {
+                        *out = value;
+                    }
+                }
+            },
+            cpal::StreamData::Output { buffer: cpal::UnknownTypeBuffer::I16(mut buffer) } => {
+                for sample in buffer.chunks_mut(format.channels as usize) {
+                    let value = (next_value() * std::i16::MAX as f32) as i16;
+                    for out in sample.iter_mut() {
+                        *out = value;
+                    }
+                }
+            },
+            cpal::StreamData::Output { buffer: cpal::UnknownTypeBuffer::F32(mut buffer) } => {
+                for sample in buffer.chunks_mut(format.channels as usize) {
+                    let value = next_value();
+                    for out in sample.iter_mut() {
+                        *out = value;
+                    }
                 }
             },
             _ => (),
