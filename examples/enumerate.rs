b07a5453b8f6f1f4e608ccc7eb667ff6f765c538
mitchmindtree <mitchell.nordine@gmail.com>
2018-02-05 15:35:40 +1100

    Update enumerate.rs example to provide more detail about devices
    The enumerate.rs example now also displays:
    
    - Supported input stream formats.
    - Default input stream format.
    - Default output stream format.
    
    This should also be useful for testing the progress of #201.

--- examples/enumerate.rs
+++ examples/enumerate.rs
@@ -1,25 +1,50 @@
 extern crate cpal;
 
 fn main() {
+    println!("Default Input Device:\n  {:?}", cpal::default_input_device().map(|e| e.name()));
     println!("Default Output Device:\n  {:?}", cpal::default_output_device().map(|e| e.name()));
 
     let devices = cpal::devices();
     println!("Devices: ");
     for (device_index, device) in devices.enumerate() {
-        println!("{}. device \"{}\" Output stream formats: ",
+        println!("{}. \"{}\"",
                  device_index + 1,
                  device.name());
 
-        let output_formats = match device.supported_output_formats() {
-            Ok(f) => f,
+        // Input formats
+        if let Ok(fmt) = device.default_input_format() {
+            println!("  Default input stream format:\n    {:?}", fmt);
+        }
+        let mut input_formats = match device.supported_input_formats() {
+            Ok(f) => f.peekable(),
             Err(e) => {
                 println!("Error: {:?}", e);
                 continue;
             },
         };
+        if input_formats.peek().is_some() {
+            println!("  All supported input stream formats:");
+            for (format_index, format) in input_formats.enumerate() {
+                println!("    {}.{}. {:?}", device_index + 1, format_index + 1, format);
+            }
+        }
 
-        for (format_index, format) in output_formats.enumerate() {
-            println!("{}.{}. {:?}", device_index + 1, format_index + 1, format);
+        // Output formats
+        if let Ok(fmt) = device.default_output_format() {
+            println!("  Default output stream format:\n    {:?}", fmt);
+        }
+        let mut output_formats = match device.supported_output_formats() {
+            Ok(f) => f.peekable(),
+            Err(e) => {
+                println!("Error: {:?}", e);
+                continue;
+            },
+        };
+        if output_formats.peek().is_some() {
+            println!("  All supported output stream formats:");
+            for (format_index, format) in output_formats.enumerate() {
+                println!("    {}.{}. {:?}", device_index + 1, format_index + 1, format);
+            }
         }
     }
 }
