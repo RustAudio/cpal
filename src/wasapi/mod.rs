c84ca77cb7f8d66b2fea28ad3e4c120c84646162
mitchmindtree <mitchell.nordine@gmail.com>
2018-02-07 21:33:09 +1100

    Update winapi version from 0.2 to 0.3
    Adds only the necessary cargo features to reduce compile time and reduce
    the chance of linking errors occurring for unused libraries (e.g.
    d3d12.dll fails to link on my win10 VM).
    
    I thought I'd try and land this before before working on the wasapi
    backend implementation for #201.
    
    Tested both beep.rs and enumerate.rs and they work fine with the update.

--- src/wasapi/mod.rs
+++ src/wasapi/mod.rs
@@ -1,18 +1,17 @@
 extern crate winapi;
-extern crate ole32;
-extern crate kernel32;
 
 use std::io::Error as IoError;
 
 pub use self::endpoint::{Endpoint, EndpointsIterator, SupportedFormatsIterator, default_endpoint};
 pub use self::voice::{Buffer, EventLoop, VoiceId};
+use self::winapi::um::winnt::HRESULT;
 
 mod com;
 mod endpoint;
 mod voice;
 
 #[inline]
-fn check_result(result: winapi::HRESULT) -> Result<(), IoError> {
+fn check_result(result: HRESULT) -> Result<(), IoError> {
     if result < 0 {
         Err(IoError::from_raw_os_error(result))
     } else {
