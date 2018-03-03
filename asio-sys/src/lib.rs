92e96712e72be753e734d10acb538b8705c60b72
Tom Gowan <s3549186@student.rmit.edu.au>
2018-03-03 16:22:24 +1100

    asio refactor v0.3

--- asio-sys/src/lib.rs
+++ asio-sys/src/lib.rs
@@ -1,12 +1,13 @@
 mod asio_import;
 
-use std::mem;
 use std::os::raw::c_char;
 use std::ffi::CStr;
 use std::ffi::CString;
 use std::os::raw::c_long;
 
 use asio_import as ai;
+
+const MAX_DRIVER: i32 = 32;
 
 fn setup(){
     /*
@@ -19,10 +20,10 @@ fn setup(){
         if load_result {
             let mut ins: c_long = 0;
             let mut outs: c_long = 0;
-            let mut driver_info = sys::ASIODriverInfo{_bindgen_opaque_blob: [0u32; 43] };
-            let init_result = sys::ASIOInit(&mut driver_info);
+            let mut driver_info = ai::ASIODriverInfo{_bindgen_opaque_blob: [0u32; 43] };
+            let init_result = ai::ASIOInit(&mut driver_info);
             println!("init result: {}", init_result);
-            let channel_result: sys::ASIOError = sys::ASIOGetChannels(&mut ins, &mut outs);
+            let channel_result: ai::ASIOError = ai::ASIOGetChannels(&mut ins, &mut outs);
             println!("channel result: {}", channel_result);
             println!("ins: {}", ins);
             println!("outs: {}", outs);
@@ -36,17 +37,16 @@ fn setup(){
 pub fn get_driver_list() -> Vec<String>{
     let driver_list: Vec<String> = Vec::new();
 
-    let max_names = max_names;
-    let mut driver_names: [[c_char; max_names]; max_names] = [[0; max_names]; max_names];
-    let mut p_driver_name: [*mut i8; max_names] = [0 as *mut i8; max_names];
+    let mut driver_names: [[c_char; MAX_DRIVER]; MAX_DRIVER] = [[0; MAX_DRIVER]; MAX_DRIVER];
+    let mut p_driver_name: [*mut i8; MAX_DRIVER] = [0 as *mut i8; MAX_DRIVER];
 
-    for i in 0..max_names{
+    for i in 0..MAX_DRIVER{
         p_driver_name[i] = driver_names[i].as_mut_ptr();
     }
 
-    let mut asio_drivers = sys::AsioDrivers::new();
+    let mut asio_drivers = ai::AsioDrivers::new();
 
-    let num_drivers = asio_drivers.getDriverNames(p_driver_name.as_mut_ptr(), max_names as i32);
+    let num_drivers = asio_drivers.getDriverNames(p_driver_name.as_mut_ptr(), MAX_DRIVER as i32);
 
     if num_drivers > 0{
         println!("found driver");
@@ -63,7 +63,9 @@ pub fn get_driver_list() -> Vec<String>{
         println!("no result");
     }
 
-    sys::destruct_AsioDrivers(&mut asio_drivers);
+    ai::destruct_AsioDrivers(&mut asio_drivers);
+
+    driver_list
 }
 
 
