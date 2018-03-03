2ed1f791228614389631e222c48a24c339ada55b
Tom Gowan <s3549186@student.rmit.edu.au>
2018-03-03 16:28:45 +1100

    asio refactor v0.7

--- asio-sys/examples/test.rs
+++ asio-sys/examples/test.rs
@@ -1,7 +1,7 @@
 extern crate asio_sys as sys;
 
 fn main() {
-    let driver_list = sys::get_driver_names();
+    let driver_list = sys::get_driver_list();
 
     for driver in driver_list{
         println!("Driver: {}", driver);
