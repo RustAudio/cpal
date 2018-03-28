4ddf2da60b9cec68cd70146ba2559f34d481f38b
DMSDeveloper <dmsdeveloper@mvms.vic.gov.au>
2018-03-28 14:24:52 +1100

    derives v2

--- src/platform/windows/asio/mod.rs
+++ src/platform/windows/asio/mod.rs
@@ -1,9 +1,23 @@
 extern crate asio_sys as sys;
 
+use CreationError;
+use DefaultFormatError;
+use FormatsEnumerationError;
+
+use Format;
+
 pub struct Devices{
 }
 
 pub struct Device;
+
+pub struct EventLoop;
+
+pub struct SupportedInputFormats;
+pub struct SupportedOutputFormats;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct StreamId(usize);
 
 impl Default for Devices {
     fn default() -> Devices {
@@ -15,5 +29,50 @@ impl Device {
     pub fn name(&self) -> String {
         "".to_owned()
     }
+    
+    pub fn supported_input_formats(&self) -> Result<SupportedInputFormats, 
+    FormatsEnumerationError> {
+        unimplemented!()
+    }
+    
+    pub fn supported_output_formats(&self) -> Result<SupportedOutputFormats, 
+    FormatsEnumerationError> {
+        unimplemented!()
+    }
+    
+    pub fn default_input_format(&self) -> Result<Format, DefaultFormatError> {
+        unimplemented!()
+    }
+    
+    pub fn default_output_format(&self) -> Result<Format, DefaultFormatError> {
+        unimplemented!()
+    }
 }
 
+impl EventLoop {
+    pub fn new() -> EventLoop {
+        EventLoop
+    }
+
+    pub fn build_input_stream(
+        &self,
+        device: &Device,
+        format: &Format,
+    ) -> Result<StreamId, CreationError>
+    {
+        unimplemented!()
+    }
+
+    pub fn build_output_stream(
+        &self,
+        device: &Device,
+        format: &Format,
+    ) -> Result<StreamId, CreationError>
+    {
+        unimplemented!()
+    }
+    
+    pub fn play_stream(&self, stream: StreamId) {
+        unimplemented!()
+    }
+}
--- src/platform/windows/mod.rs
+++ src/platform/windows/mod.rs
@@ -1,5 +1,11 @@
 use os::windows::{Backend, which_backend};
 
+use CreationError;
+use DefaultFormatError;
+use FormatsEnumerationError;
+
+use Format;
+
 #[cfg(windows)]
 mod asio;
 
@@ -8,7 +14,7 @@ mod wasapi;
 
 pub enum EventLoop {
     Wasapi(wasapi::EventLoop),
-    Asio,
+    Asio(asio::EventLoop),
 }
 
 #[derive(Clone, PartialEq, Eq)]
@@ -20,7 +26,7 @@ pub enum Device {
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum StreamId{
     Wasapi(wasapi::StreamId),
-    Asio
+    Asio(asio::StreamId),
 }
 
 pub enum InputBuffer<'a, T: 'a>{
@@ -40,12 +46,12 @@ pub enum Devices{
 
 pub enum SupportedInputFormats{
     Wasapi(wasapi::SupportedInputFormats),
-    Asio,
+    Asio(asio::SupportedInputFormats),
 }
 
 pub enum SupportedOutputFormats{
     Wasapi(wasapi::SupportedOutputFormats),
-    Asio,
+    Asio(asio::SupportedOutputFormats),
 }
 
 pub fn default_input_device() -> Option<Device> {
@@ -76,6 +82,77 @@ impl Device {
         match self {
             &Device::Wasapi(ref d) => d.name(),
             &Device::Asio(ref d) => d.name(),
+        }
+    }
+    
+    pub fn supported_input_formats(&self) -> Result<SupportedInputFormats, 
+    FormatsEnumerationError> {
+        match self {
+            &Device::Wasapi(ref d) => d.supported_input_formats(),
+            &Device::Asio(ref d) => d.supported_input_formats(),
+        }
+    }
+    
+    pub fn supported_output_formats(&self) -> Result<SupportedOutputFormats, 
+    FormatsEnumerationError> {
+        match self {
+            &Device::Wasapi(ref d) => d.supported_output_formats(),
+            &Device::Asio(ref d) => d.supported_output_formats(),
+        }
+    }
+
+    pub fn default_input_format(&self) -> Result<Format, DefaultFormatError> {
+        match self {
+            &Device::Wasapi(ref d) => d.default_input_format(),
+            &Device::Asio(ref d) => d.default_input_format(),
+        }
+    }
+    
+    pub fn default_output_format(&self) -> Result<Format, DefaultFormatError> {
+        match self {
+            &Device::Wasapi(ref d) => d.default_output_format(),
+            &Device::Asio(ref d) => d.default_output_format(),
+        }
+    }
+}
+
+
+impl EventLoop {
+    pub fn new() -> EventLoop {
+        match which_backend() {
+            Backend::Wasapi => wasapi::EventLoop::new(),
+            Backend::Asio => asio::EventLoop::new(),
+        }
+    }
+    
+    pub fn build_input_stream(
+        &self,
+        device: &Device,
+        format: &Format,
+    ) -> Result<StreamId, CreationError>
+    {
+        match self {
+            &EventLoop::Wasapi(ref d) => d.build_input_stream(device, format),
+            &EventLoop::Asio(ref d) => d.build_input_stream(device, format),
+        }
+    }
+    
+    pub fn build_output_stream(
+        &self,
+        device: &Device,
+        format: &Format,
+    ) -> Result<StreamId, CreationError>
+    {
+        match self {
+            &EventLoop::Wasapi(ref d) => d.build_output_stream(device, format),
+            &EventLoop::Asio(ref d) => d.build_output_stream(device, format),
+        }
+    }
+    
+    pub fn play_stream(&self, stream: StreamId) {
+        match self {
+            &EventLoop::Wasapi(ref d) => d.play_stream(stream),
+            &EventLoop::Asio(ref d) => d.play_stream(stream),
         }
     }
 }
