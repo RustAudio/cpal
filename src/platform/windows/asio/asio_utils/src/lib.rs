056630cca37ee3e2a0a076e50b06bcb3c419311b
Tom Gowan <tomrgowan@gmail.com>
2018-10-12 23:38:55 +1100

    removed allocation from output

--- src/platform/windows/asio/asio_utils/src/lib.rs
+++ src/platform/windows/asio/asio_utils/src/lib.rs
@@ -1,7 +1,3 @@
-use std::cell::RefCell;
-use std::iter::Cloned;
-use std::slice::{Iter, IterMut};
-
 #[cfg(test)]
 mod tests;
 
@@ -22,27 +18,6 @@ where
     let frames = channels[0].len();
     target.extend((0 .. frames).flat_map(|f| channels.iter().map(move |ch| ch[f])));
 }
-/*
-pub fn interleave<T>(channel_buffer: &[Vec<T>], cpal_buffer: &mut [T])
-where
-    T: std::marker::Copy,
-{
-    // TODO avoid this heap allocation
-    // But we don't know how many channels we need.
-    // Could use arrayvec if we make an upper limit
-    let channels: Vec<RefCell<Cloned<Iter<T>>>> = channel_buffer
-        .iter()
-        .map(|c| RefCell::new(c.iter().cloned()))
-        .collect();
-
-    for (c_buff, channel) in cpal_buffer.iter_mut().zip(channels.iter().cycle()) {
-        match channel.borrow_mut().next() {
-            Some(c) => *c_buff = c,
-            None => break,
-        }
-    }
-}
-*/
 
 /// Function for deinterleaving because
 /// cpal writes to buffer interleaved
@@ -64,23 +39,3 @@ where
         asio_channels[ch.next().unwrap()].push(sample);
     }
 }
-/*
-pub fn deinterleave<T>(cpal_buffer: &[T], asio_channels: &mut [Vec<T>])
-where
-    T: std::marker::Copy,
-{
-    // TODO avoid this heap allocation
-    // Possibly use arrayvec and some max channels
-    let channels: Vec<RefCell<IterMut<T>>> = asio_channels
-        .iter_mut()
-        .map(|c| RefCell::new(c.iter_mut()))
-        .collect();
-
-    for (c_buff, a_channel) in cpal_buffer.iter().zip(channels.iter().cycle()) {
-        match a_channel.borrow_mut().next() {
-            Some(c) => *c = *c_buff,
-            None => break,
-        }
-    }
-}
-*/
