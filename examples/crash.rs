3eecb805fcc03fc1bb6e58650ebdf2a8e6af8be7
MOZGIII <mike-n@narod.ru>
2019-03-31 15:46:56 +0300

    Added crash example

---
+++ examples/crash.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let input_device = cpal::default_input_device().unwrap();
+    let input_format = input_device.default_input_format().unwrap();
+
+    let event_loop = cpal::EventLoop::new();
+
+    let input_stream_id = event_loop.build_input_stream(&input_device, &input_format).unwrap();
+    event_loop.play_stream(input_stream_id);
+
+    event_loop.run(move |_stream_id, stream_data| {
+        match stream_data {
+            cpal::StreamData::Input { buffer: _ } => {
+                // not even use buffer
+            }
+            _ => unreachable!(),
+        }
+    });
+}
