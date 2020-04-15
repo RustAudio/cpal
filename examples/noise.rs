41913d7263f83b4b53ca4c6cff07d96ff69019e2
Richard Dodd <richard.o.dodd@gmail.com>
2020-04-15 19:05:53 +0100

    Add noise example

---
+++ examples/noise.rs
@@ -0,0 +1,59 @@
+extern crate anyhow;
+extern crate cpal;
+extern crate rand;
+
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+use rand::prelude::*;
+
+fn main() -> Result<(), anyhow::Error> {
+    let host = cpal::default_host();
+    let device = host
+        .default_output_device()
+        .expect("failed to find a default output device");
+    let config = device.default_output_config()?;
+
+    match config.sample_format() {
+        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into())?,
+        cpal::SampleFormat::I16 => run::<i16>(&device, &config.into())?,
+        cpal::SampleFormat::U16 => run::<u16>(&device, &config.into())?,
+    }
+
+    Ok(())
+}
+
+fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error>
+where
+    T: cpal::Sample,
+{
+    let channels = config.channels as usize;
+
+    let mut rng = SmallRng::from_entropy();
+    let mut next_value = move || {
+        rng.gen()
+    };
+
+    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+
+    let stream = device.build_output_stream(
+        config,
+        move |data: &mut [T]| write_data(data, channels, &mut next_value),
+        err_fn,
+    )?;
+    stream.play()?;
+
+    std::thread::sleep(std::time::Duration::from_millis(10000));
+
+    Ok(())
+}
+
+fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
+where
+    T: cpal::Sample,
+{
+    for frame in output.chunks_mut(channels) {
+        let value: T = cpal::Sample::from::<f32>(&next_sample());
+        for sample in frame.iter_mut() {
+            *sample = value;
+        }
+    }
+}
