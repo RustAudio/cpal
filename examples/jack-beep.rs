18bd609cca690a617522549dc31926ffc1883411
Erik Natanael Gustafsson <erik@eriknatanael.com>
2020-09-15 14:51:32 +0200

    Add jack examples to make it easy to test

---
+++ examples/jack-beep.rs
@@ -0,0 +1,69 @@
+extern crate cpal;
+
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    HostId,
+};
+
+fn main() -> Result<(), anyhow::Error> {
+    let host = cpal::host_from_id(cpal::available_hosts()
+        .into_iter()
+        .find(|id| *id == HostId::Jack)
+        .expect(
+            "make sure --features jack is specified. only works on OSes where jack is available",
+        )).expect("jack host unavailable");
+    let device = host
+        .default_output_device()
+        .expect("failed to find a default output device");
+    let config = device.default_output_config()?;
+
+    match config.sample_format() {
+        cpal::SampleFormat::F32 => run::<f32>(&device, &config.into())?,
+        _ => panic!("only F32 supported on jack"),
+    }
+
+    Ok(())
+}
+
+fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error>
+where
+    T: cpal::Sample,
+{
+    let sample_rate = config.sample_rate.0 as f32;
+    let channels = config.channels as usize;
+
+    // Produce a sinusoid of maximum amplitude.
+    let mut sample_clock = 0f32;
+    let mut next_value = move || {
+        sample_clock = (sample_clock + 1.0) % sample_rate;
+        (sample_clock * 440.0 * 2.0 * 3.141592 / sample_rate).sin()
+    };
+
+    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+
+    let stream = device.build_output_stream(
+        config,
+        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
+            write_data(data, channels, &mut next_value)
+        },
+        err_fn,
+    )?;
+    stream.play()?;
+
+    std::thread::sleep(std::time::Duration::from_millis(1_000_000));
+
+    Ok(())
+}
+
+fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32)
+where
+    T: cpal::Sample,
+{
+    //println!();
+    for frame in output.chunks_mut(channels) {
+        let value: T = cpal::Sample::from::<f32>(&next_sample());
+        for sample in frame.iter_mut() {
+            *sample = value;
+        }
+    }
+}
