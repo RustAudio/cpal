5962e25f7c6af6cbb05897b48171179970d3a67e
Tyler Schmidt <tmschmid@protonmail.com>
2021-05-23 10:51:39 -0700

    Changed ringbuf to synchronized mpsc in ios-feedback example as well. Removed reference to ringbuf from Cargo.toml.

--- examples/ios-feedback/src/feedback.rs
+++ examples/ios-feedback/src/feedback.rs
@@ -8,10 +8,9 @@
 
 extern crate anyhow;
 extern crate cpal;
-extern crate ringbuf;
 
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use ringbuf::RingBuffer;
+use std::sync::mpsc;
 
 const LATENCY_MS: f32 = 1000.0;
 
@@ -35,45 +34,17 @@ pub fn run_example() -> Result<(), anyhow::Error> {
     let latency_frames = (LATENCY_MS / 1_000.0) * config.sample_rate.0 as f32;
     let latency_samples = latency_frames as usize * config.channels as usize;
 
-    // The buffer to share samples
-    let ring = RingBuffer::new(latency_samples * 2);
-    let (mut producer, mut consumer) = ring.split();
-
-    // Fill the samples with 0.0 equal to the length of the delay.
-    for _ in 0..latency_samples {
-        // The ring buffer has twice as much space as necessary to add latency here,
-        // so this should never fail
-        producer.push(0.0).unwrap();
-    }
+    let (tx, rx) = mpsc::sync_channel(latency_samples * 2);
 
     let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
-        let mut output_fell_behind = false;
         for &sample in data {
-            if producer.push(sample).is_err() {
-                output_fell_behind = true;
-            }
-        }
-        if output_fell_behind {
-            eprintln!("output stream fell behind: try increasing latency");
+            tx.send(sample).unwrap();
         }
     };
 
     let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-        let mut input_fell_behind = None;
         for sample in data {
-            *sample = match consumer.pop() {
-                Ok(s) => s,
-                Err(err) => {
-                    input_fell_behind = Some(err);
-                    0.0
-                }
-            };
-        }
-        if let Some(err) = input_fell_behind {
-            eprintln!(
-                "input stream fell behind: {:?}: try increasing latency",
-                err
-            );
+            *sample = rx.recv().unwrap_or(0.0);
         }
     };
 
