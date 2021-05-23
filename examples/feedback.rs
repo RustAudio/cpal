982871db7d09e55e1906bea49f3e51292a35d9b0
Tyler Schmidt <tmschmid@protonmail.com>
2021-05-23 10:40:19 -0700

    Changed ringbuffer usage to synchronized mpsc.

--- examples/feedback.rs
+++ examples/feedback.rs
@@ -9,11 +9,10 @@
 extern crate anyhow;
 extern crate clap;
 extern crate cpal;
-extern crate ringbuf;
 
 use anyhow::Context;
 use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
-use ringbuf::RingBuffer;
+use std::sync::mpsc;
 
 #[derive(Debug)]
 struct Opt {
@@ -128,42 +127,17 @@ fn main() -> anyhow::Result<()> {
     let latency_frames = (opt.latency / 1_000.0) * config.sample_rate.0 as f32;
     let latency_samples = latency_frames as usize * config.channels as usize;
 
-    // The buffer to share samples
-    let ring = RingBuffer::new(latency_samples * 2);
-    let (mut producer, mut consumer) = ring.split();
-
-    // Fill the samples with 0.0 equal to the length of the delay.
-    for _ in 0..latency_samples {
-        // The ring buffer has twice as much space as necessary to add latency here,
-        // so this should never fail
-        producer.push(0.0).unwrap();
-    }
+    let (tx, rx) = mpsc::sync_channel(latency_samples * 2);
 
     let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
-        let mut output_fell_behind = false;
         for &sample in data {
-            if producer.push(sample).is_err() {
-                output_fell_behind = true;
-            }
-        }
-        if output_fell_behind {
-            eprintln!("output stream fell behind: try increasing latency");
+            tx.send(sample).unwrap();
         }
     };
 
     let output_data_fn = move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
-        let mut input_fell_behind = false;
         for sample in data {
-            *sample = match consumer.pop() {
-                Some(s) => s,
-                None => {
-                    input_fell_behind = true;
-                    0.0
-                }
-            };
-        }
-        if input_fell_behind {
-            eprintln!("input stream fell behind: try increasing latency");
+            *sample = rx.recv().unwrap_or(0.0);
         }
     };
 
