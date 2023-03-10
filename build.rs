bc3af933126ab265fc951773d628e86f36663f7e
Julian Bayardo Spadafora <julian@bayardo.info>
2023-03-09 19:15:30 -0800

    Formatter

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ const CPAL_ASIO_DIR: &str = "CPAL_ASIO_DIR";
 
 fn main() {
     println!("cargo:rerun-if-env-changed={}", CPAL_ASIO_DIR);
-    
+
     // If ASIO directory isn't set silently return early
     // otherwise set the asio config flag
     match env::var(CPAL_ASIO_DIR) {
