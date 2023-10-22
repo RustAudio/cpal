61c13860ea5d3aafc18f7c4fd4a22e8194d3de28
Liam Germain <79514573+PhilosophicalProgrammer@users.noreply.github.com>
2023-10-22 15:22:58 -0500

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,9 +1,13 @@
 use std::env;
-
 const CPAL_ASIO_DIR: &str = "CPAL_ASIO_DIR";
 
 fn main() {
     println!("cargo:rerun-if-env-changed={}", CPAL_ASIO_DIR);
+
+    // This avoids runtime errors on Android targets.
+    if std::env::var("TARGET").unwrap().contains("android") {
+        println!("cargo:rustc-link-lib=c++_shared");
+    }
 
     // If ASIO directory isn't set silently return early
     // otherwise set the asio config flag
