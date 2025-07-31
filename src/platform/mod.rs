2a4b3756c5eb6b62b044a66600332fce774dd61b
TD-Sky <three-dim-sky@foxmail.com>
2025-07-31 17:43:30 +0800

    fix lost feature

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -708,6 +708,7 @@ mod platform_impl {
         Stream as AAudioStream, SupportedInputConfigs as AAudioSupportedInputConfigs,
         SupportedOutputConfigs as AAudioSupportedOutputConfigs,
     };
+    #[cfg(feature = "android-input-preset")]
     pub use ndk::audio::AudioInputPreset;
 
     impl_platform_host!(AAudio aaudio "AAudio");
