100db1a2cc4b4c9967441d3fdd281fdda2bfdc2f
Jason Calderwood <jason@aaware.com>
2025-12-04 17:08:31 -0500

    Revert "fix: improve sample rate validation logic"
    This reverts commit d415918bcdb88d2222c0fd28a5d287e74e1dd4c1.

--- src/host/coreaudio/macos/device.rs
+++ src/host/coreaudio/macos/device.rs
@@ -108,10 +108,10 @@ fn set_sample_rate(
 
         // Now that we have the available ranges, pick the one matching the desired rate.
         let sample_rate = target_sample_rate.0;
-        if !ranges
+        let maybe_index = ranges
             .iter()
-            .any(|r| sample_rate as f64 >= r.mMinimum && sample_rate as f64 <= r.mMaximum)
-        {
+            .position(|r| r.mMinimum as u32 == sample_rate && r.mMaximum as u32 == sample_rate);
+        if maybe_index.is_none() {
             return Err(BuildStreamError::StreamConfigNotSupported);
         }
 
