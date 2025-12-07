c240ecb70b502a836ebd4551c5c5d4e4b86d5a04
Sintel <sintel.inquiries@gmail.com>
2025-12-07 18:54:34 +0100

    shouldn't remove the cfg(asio)

--- asio-sys/src/lib.rs
+++ asio-sys/src/lib.rs
@@ -4,6 +4,9 @@
 extern crate num_derive;
 extern crate num_traits;
 
+#[cfg(asio)]
 pub mod bindings;
+#[cfg(asio)]
 pub use bindings::errors::{AsioError, LoadDriverError};
+#[cfg(asio)]
 pub use bindings::*;
