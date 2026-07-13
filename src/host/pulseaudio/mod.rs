baefebf02981bc21e3f4bd31d80557751a4f45c4
Roderick van Domburg <roderick@vandomburg.net>
2026-07-13 07:37:19 +0200

    fix(pulseaudio): map NoData to BackendError instead of Xrun

--- src/host/pulseaudio/mod.rs
+++ src/host/pulseaudio/mod.rs
@@ -94,7 +94,6 @@ impl From<pulseaudio::ClientError> for Error {
                 ConnectionTerminated | Killed | Protocol | BadState | Io => {
                     ErrorKind::StreamInvalidated
                 }
-                NoData => ErrorKind::Xrun,
                 _ => ErrorKind::BackendError,
             }
         }
