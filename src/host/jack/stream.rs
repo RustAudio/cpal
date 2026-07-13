10a0266b1a396d91d0e1f34a7e258c66ae55e4e8
Roderick van Domburg <roderick@vandomburg.net>
2026-07-13 23:19:16 +0200

    refactor(jack): use f32::EQUILIBRIUM instead of 0.0

--- src/host/jack/stream.rs
+++ src/host/jack/stream.rs
@@ -304,8 +304,8 @@ impl LocalProcessHandler {
         pending_xrun: Arc<AtomicBool>,
         #[cfg(feature = "realtime")] error_callback: ErrorCallbackArc,
     ) -> Self {
-        let temp_input_buffer = vec![0.0; in_ports.len() * buffer_size];
-        let temp_output_buffer = vec![0.0; out_ports.len() * buffer_size];
+        let temp_input_buffer = vec![f32::EQUILIBRIUM; in_ports.len() * buffer_size];
+        let temp_output_buffer = vec![f32::EQUILIBRIUM; out_ports.len() * buffer_size];
 
         Self {
             out_ports,
@@ -531,8 +531,8 @@ impl jack::ProcessHandler for LocalProcessHandler {
         let new_size = size as usize;
         if new_size != self.buffer_size {
             self.buffer_size = new_size;
-            self.temp_input_buffer = vec![0.0; self.in_ports.len() * new_size];
-            self.temp_output_buffer = vec![0.0; self.out_ports.len() * new_size];
+            self.temp_input_buffer = vec![f32::EQUILIBRIUM; self.in_ports.len() * new_size];
+            self.temp_output_buffer = vec![f32::EQUILIBRIUM; self.out_ports.len() * new_size];
         }
 
         jack::Control::Continue
