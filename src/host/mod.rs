c4a92cb09c35983685727c8984f39b901e3e012f
Roderick van Domburg <roderick@vandomburg.net>
2026-07-29 23:26:02 +0200

    fix: include *BSD in frames_to_duration gate
    Fixes #1294

--- src/host/mod.rs
+++ src/host/mod.rs
@@ -231,20 +231,12 @@ pub(crate) use error_emit::try_emit_error;
 /// Convert a frame count at a given sample rate to a [`std::time::Duration`].
 #[cfg(any(
     target_os = "linux",
+    target_os = "dragonfly",
+    target_os = "freebsd",
+    target_os = "netbsd",
     target_os = "windows",
     target_vendor = "apple",
     feature = "audioworklet",
-    all(
-        feature = "jack",
-        any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd",
-            target_os = "macos",
-            target_os = "windows",
-        )
-    )
 ))]
 #[inline]
 pub(crate) fn frames_to_duration(
